f974d6f9cc912bb130d72aa1ddfb653f5cd5e76f
Mustafa Dalga <mustafadalgaa@gmail.com>
2023-09-25 12:33:40 +0300

    cell position,cell and letter types have been defined

--- app/_types/index.ts
+++ app/_types/index.ts
@@ -1,10 +1,15 @@
 export type Difficulty = "easy" | "medium" | "hard";
-export type Board = (string | null)[][];
+export type Board = (Cell | null)[][];
 export type Direction = (string | null)[];
-export type Position = [number, number]
+export type Letter = "A" | "B" | "C"
+export type Cell = Letter | null
 export type Hints = {
     top: Direction,
     bottom: Direction,
     left: Direction,
     right: Direction
 };
+export type CellPosition = {
+    row: number,
+    col: number
+}
